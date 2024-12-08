3f0dafb8bd893945c5c0e9e201ea005ed7808bba
jgaik <jakub.gaik@gmail.com>
2024-12-08 01:34:43 +0100

    add yamori design styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app.tsx";
 import "./index.scss";
+import "@yamori-design/styles/dist/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
