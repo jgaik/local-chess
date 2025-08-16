47bd9fa76d7dd0f51b48dbeaffcdad34fba9215d
Jakub Gaik <jakub.gaik@gmail.com>
2025-08-16 15:03:50 +0900

    update packages

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/local-chess/",
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler",
-      },
-    },
-  },
 });
